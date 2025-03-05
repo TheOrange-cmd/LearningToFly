1fc60054f44dcf2b95ebe9c4ccd23b7424e0419a
Twan <twanvandam02@gmail.com>
2025-03-05 20:06:28 +0100

    Update model to take images straight from the camera. Inputs is now (1,3,520,240) tensor with values expected input values between 0-255. Output is a (1,5) tensor

--- SmallConvNetwork/CNN.py
+++ SmallConvNetwork/CNN.py
@@ -1,5 +1,6 @@
 import torchvision.io
 import glob
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -28,57 +29,56 @@ class CustomImageDataset(Dataset):
         return bboxes
 
     def __getitem__(self, idx):
-        image = torchvision.io.read_image(self.image_paths[idx]).float().to("cuda") / 255
+        image = torchvision.io.read_image(self.image_paths[idx]).float().cuda()
+        if image.shape[-2:] != (self.width, self.height):
+            raise ValueError(f"Image shape {image.shape} of {self.image_paths[idx]} is not equal to the expected shape ({self.width}, {self.height})")
         bboxes = self.get_bboxes(self.label_paths[idx])
         cell_danger_levels = torch.tensor(cell_contains_bbox(bboxes, grid_lines)).float()
         return image, cell_danger_levels
 
 class ObjectDetectionModel(pl.LightningModule):
-    def __init__(self, grid_lines, danger_levels):
+    def __init__(self, grid_lines):
         super(ObjectDetectionModel, self).__init__()
         self.grid_lines = grid_lines
-        self.danger_levels = danger_levels
-        self.model = torch.nn.Sequential(
-            # input shape: 3x240x520
-            torch.nn.MaxPool2d(3),
 
-            # input shape: 3x80x173
+        self.conv = torch.nn.Sequential(
+            torch.nn.MaxPool2d(4),
+
+            # input shape: 3x130x60
             torch.nn.Conv2d(3, 16, 3, padding=1, stride=2),
             torch.nn.ReLU(),
 
-            # input shape: 16x40x87
-            torch.nn.Conv2d(16, 64, 3, padding=1, stride=2),
-            torch.nn.ReLU(),
-
-            # input shape: 64x20x44
-            torch.nn.Conv2d(64, 256, 3, padding=1),
-            torch.nn.ReLU(),
-
-            # input shape: 256x20x44
-            torch.nn.Conv2d(256, 64, 1, padding=0),
+            # input shape: 16x65x30
+            torch.nn.Conv2d(16, 64, 3, padding=1, stride=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
 
-            # input shape: 64x10x22
-            torch.nn.Flatten(),
-
-            # input shape: 64x10x22
-            torch.nn.LazyLinear(1024),
+            # input shape: 64x33x15
+            torch.nn.Conv2d(64, 128, 3, padding=1, stride=2),
             torch.nn.ReLU(),
 
-            # input shape: 1024
-            torch.nn.LazyLinear(256),
+            # input shape: 128x17x8
+            torch.nn.Conv2d(128, 32, 1, padding=0, stride=1),
             torch.nn.ReLU(),
-
-            # input shape: 256
-            torch.nn.LazyLinear(danger_levels * (len(grid_lines[0]) - 1) * (len(grid_lines[1]) - 1)),
-            torch.nn.Sigmoid(),
-            torch.nn.Unflatten(1, (danger_levels, len(grid_lines[0]) - 1, len(grid_lines[1]) - 1))
+            torch.nn.MaxPool2d(2),
+            #output shape: 32x8x4
         )
-        self.loss_fn = torch.nn.BCELoss()
+
+        self.fc = torch.nn.Sequential(
+            torch.nn.Linear(32*8*4, 128),
+            torch.nn.ReLU(),
+
+            torch.nn.Linear(128,len(grid_lines) - 1)
+        )
+        self.loss_fn = torch.nn.MSELoss()
 
     def forward(self, x):
-        return self.model(x)
+        x = x / 255
+        x = self.conv(x)
+        x = x.view(x.size(0),-1)
+        x = self.fc(x)
+        x = x.view(x.size(0), len(self.grid_lines) - 1)
+        return x
 
     def training_step(self, batch, batch_idx):
         images, targets = batch
@@ -98,7 +98,7 @@ class ObjectDetectionModel(pl.LightningModule):
         return torch.optim.Adam(self.parameters(), lr=0.001)
 
 class ObjectDetectionDataModule(pl.LightningDataModule):
-    def __init__(self, image_dir_train, image_dir_val, width, height, batch_size=8):
+    def __init__(self, image_dir_train, image_dir_val, width, height, batch_size=16):
         super(ObjectDetectionDataModule, self).__init__()
         self.image_dir_train = image_dir_train
         self.image_dir_val = image_dir_val
@@ -125,26 +125,15 @@ def convert_coordinates(bbox):
     return x1, x2, y1, y2
 
 def cell_contains_bbox(bboxes, grid_lines):
-    box_inside_grid = [[[0 for _ in range(len(grid_lines[1])-1)] for _ in range(len(grid_lines[0])-1)] for _ in range(danger_levels)]
+    danger_list = [0 for _ in range(len(grid_lines)-1)]
     for bbox in bboxes:
         x1, x2, y1, y2 = convert_coordinates(bbox)
-        for i in range(len(grid_lines[0])-1):
-            left = grid_lines[0][i]
-            right = grid_lines[0][i+1]
+        for i in range(len(grid_lines)-1):
+            left = grid_lines[i]
+            right = grid_lines[i+1]
             if ((left <= x1 <= right) or (left <= x2 <= right)) or (x1 <= left and x2 >= right):
-                for j in range(len(grid_lines[1])-1):
-                    bottom = grid_lines[1][j]
-                    top = grid_lines[1][j+1]
-                    if (bottom <= y1 <= top) or (bottom <= y2 <= top):
-                        danger = danger_level(x1, x2, y1, y2, bbox["label"])
-                        box_inside_grid[0][i][j] = 1 # Safe
-                        if danger >= 0.6:
-                            box_inside_grid[1][i][j] = 1 # Warning
-                        else:
-                            continue
-                        if 0.7 <= danger <= 1:
-                            box_inside_grid[2][i][j] = 1 # Danger
-    return box_inside_grid
+                danger_list[i] = max(danger_list[i], danger_level(x1, x2, y1, y2, bbox["label"]))
+    return danger_list
 
 def danger_level(x1, x2, y1, y2, label):
     x1_rel = x1 / width
@@ -181,97 +170,54 @@ def danger_level(x1, x2, y1, y2, label):
         danger = min(max([width_distance, height_distance]) - side_distance * multiplier, 1)
     return danger
 
+def plot_image(sample_image, sample_danger, grid_lines, grid=False):
+    colors = [(0, "green"), (0.5, "orange"), (1, "red")]
+    cmap = LinearSegmentedColormap.from_list("traffic_light", colors)
 
-def plot_image(sample_image, sample_danger, truth, grid_lines, grid=False):
-    # Display the sample image.
-    plt.imshow(sample_image.squeeze(0).permute(1, 2, 0))
-    # Draw grid lines if enabled.
-    if grid:
-        for grid_line_y in grid_lines[1]:
-            plt.axhline(grid_line_y, color="blue", linestyle="--")
-        for grid_line_x in grid_lines[0]:
-            plt.axvline(grid_line_x, color="blue", linestyle="--")
+    sample_danger = sample_danger.detach().numpy()
+
+    # Rotate image to display it correctly. Divide by 255 to scale pixel values to [0, 1].
+    plt.imshow(torch.rot90(sample_image.squeeze(0), k=1, dims=[1,2]).permute(1, 2, 0)/255)
 
     # Overlay predicted danger levels.
-    for i in range(len(grid_lines[0]) - 1):
-        for j in range(len(grid_lines[1]) - 1):
-            # Prediction overlays (filled cells)
-            if sample_danger[2][i][j] > 0.8:
-                plt.fill_between([grid_lines[0][i], grid_lines[0][i + 1]],
-                                 [grid_lines[1][j], grid_lines[1][j]],
-                                 [grid_lines[1][j + 1], grid_lines[1][j + 1]],
-                                 color="red", alpha=0.5)
-            elif sample_danger[1][i][j] > 0.8:
-                plt.fill_between([grid_lines[0][i], grid_lines[0][i + 1]],
-                                 [grid_lines[1][j], grid_lines[1][j]],
-                                 [grid_lines[1][j + 1], grid_lines[1][j + 1]],
-                                 color="orange", alpha=0.5)
-            elif sample_danger[0][i][j] > 0.8:
-                plt.fill_between([grid_lines[0][i], grid_lines[0][i + 1]],
-                                 [grid_lines[1][j], grid_lines[1][j]],
-                                 [grid_lines[1][j + 1], grid_lines[1][j + 1]],
-                                 color="green", alpha=0.5)
-
-    ax = plt.gca()
-    if truth is not None:
-        for i in range(len(grid_lines[0]) - 1):
-            for j in range(len(grid_lines[1]) - 1):
-                if (truth[2][i][j] == 1):
-                    if not sample_danger[2][i][j] > 0.8:
-                        ax.add_patch(plt.Rectangle(
-                            (grid_lines[0][i], grid_lines[1][j]),
-                            grid_lines[0][i + 1] - grid_lines[0][i],
-                            grid_lines[1][j + 1] - grid_lines[1][j],
-                            linewidth=3, edgecolor="red", facecolor="none", hatch="///"))
-                elif (truth[1][i][j] == 1):
-                    if not sample_danger[1][i][j] > 0.8:
-                        ax.add_patch(plt.Rectangle(
-                            (grid_lines[0][i], grid_lines[1][j]),
-                            grid_lines[0][i + 1] - grid_lines[0][i],
-                            grid_lines[1][j + 1] - grid_lines[1][j],
-                            linewidth=3, edgecolor="orange", facecolor="none", hatch="///"))
-                elif (truth[0][i][j] == 1):
-                    if not sample_danger[0][i][j] > 0.8:
-                        ax.add_patch(plt.Rectangle(
-                            (grid_lines[0][i], grid_lines[1][j]),
-                            grid_lines[0][i + 1] - grid_lines[0][i],
-                            grid_lines[1][j + 1] - grid_lines[1][j],
-                            linewidth=3, edgecolor="green", facecolor="none", hatch="///"))
-                elif (truth[0][i][j] == 0) and (sample_danger[0][i][j] > 0.8) or (sample_danger[1][i][j] > 0.8) or (sample_danger[2][i][j] > 0.8):
-                    ax.add_patch(plt.Rectangle(
-                        (grid_lines[0][i], grid_lines[1][j]),
-                        grid_lines[0][i + 1] - grid_lines[0][i],
-                        grid_lines[1][j + 1] - grid_lines[1][j],
-                        linewidth=3, edgecolor="white", facecolor="none", hatch="///"))
+    for i in range(len(grid_lines) - 1):
+        plt.fill_between([grid_lines[i], grid_lines[i + 1]],
+                         [0, 0],
+                         [height, height],
+                         color=cmap(sample_danger[i]), alpha=0.5)
+        plt.text((grid_lines[i] +grid_lines[i + 1])/2 , height/2, f"{sample_danger[i]:.2f}", ha='center', va='center')
 
 
     plt.xlim(0, width)
     plt.ylim(height, 0)
     plt.show()
 
-image_dir_train = "./SmallConvNetwork/dataset_syn/images/train"
-image_dir_val = "./SmallConvNetwork/dataset_syn/images/val"
+image_dir_train = "./SmallConvNetwork/dataset/images/train"
+image_dir_val = "./SmallConvNetwork/dataset/images/val"
 
+# these are the dimensions of the image when it is rotated by 90 degrees, so it is displayed correctly
 width = 520
 height = 240
 
-grid_lines = [[0,0.2, 0.4,0.6, 0.8,1],[0,1]]
+grid_lines = [0,0.2, 0.4,0.6, 0.8,1]
+grid_lines = [line*width for line in grid_lines]
 
-grid_lines[0] = [line*width for line in grid_lines[0]]
-grid_lines[1] = [line*height for line in grid_lines[1]]
-
-danger_levels = 3
-
-train = False
+train = True
+save = True
 
 data_module = ObjectDetectionDataModule(image_dir_train,image_dir_val, width, height)
-model = ObjectDetectionModel(grid_lines, danger_levels)
+model = ObjectDetectionModel(grid_lines)
 
 if train:
     trainer = pl.Trainer(max_epochs=10, check_val_every_n_epoch=1, log_every_n_steps=10)
     trainer.fit(model, data_module)
+else:
+    model = ObjectDetectionModel.load_from_checkpoint("lightning_logs/version_24/checkpoints/epoch=9-step=890.ckpt", grid_lines=grid_lines)
 
-#model.eval()
+if save:
+    model.to_onnx("./SmallConvNetwork/model_rgb.onnx", torch.randn(1, 3, width, height))
+
+# plot a video to test the predictions
 model.to("cpu")
 torch.set_num_threads(1)
 plot_set = CustomImageDataset(image_dir_val,width,height)
@@ -282,24 +228,21 @@ fig, ax = plt.subplots()
 
 def update(frame):
     ax.clear()  # clear the axes for the new frame
-    image = torchvision.io.read_image(image_paths[frame]).float() / 255
+    image = torchvision.io.read_image(image_paths[frame]).float()
     start_frame = time.time()
+    print(image.unsqueeze(0).shape)
     sample_danger = model(image.unsqueeze(0)).squeeze(0)
     print(f"{1 / (time.time() - start_frame):.1f} fps")
 
     # Call the modified plot_image function that accepts an axis as parameter
     # Instead of calling plt.show() inside plot_image, pass 'ax' so that the figure updates.
-    plot_image(sample_image=image, sample_danger=sample_danger, truth=None, grid_lines=grid_lines, grid=False)
+    plot_image(sample_image=image, sample_danger=sample_danger, grid_lines=grid_lines, grid=False)
     ax.set_xlim(0, width)
     ax.set_ylim(height, 0)
     return ax
 
-# Create the figure and axis
-fig, ax = plt.subplots()
-
 # Create the animation
 ani = animation.FuncAnimation(fig, update, frames=len(image_paths), interval=100)
-
 plt.show()
 
 
